c6869efa1e25097b30ecd7a018a1afbeccef8840
Klára Madiová <madiovak@gmail.com>
2022-02-19 21:33:52 +0100

    Zpracování cvičení dle zadání

--- main.js
+++ main.js
@@ -1,1 +1,49 @@
 // tady je místo pro náš program
+let l = document.querySelector('.lion');
+let fontSize = 16;
+
+//onmouseover
+function priNajeti() {
+    l.style.fontWeight = "bold";
+}
+
+function priOdjeti() {
+    l.style.fontWeight = "normal";
+}
+
+//obarvení textu
+function isRed() {
+    l.classList.toggle('red');
+}
+
+//zvětšování písma
+function isBigger() {
+    l.style.fontSize = ++fontSize + "px";
+}
+
+//AUDIO
+let audioFile = document.getElementById('sound');
+
+function playAudio() {
+    audioFile.play();
+}
+
+function pauseAudio() {
+    audioFile.pause();
+}
+
+function minAudio() {
+    audioFile.volume = 0;
+}
+
+function mediumAudio() {
+    audioFile.volume = 0.5;
+}
+
+function maxAudio() {
+    audioFile.voluem = 1;
+}
+
+function replayAudio() {
+    audioFile.currentTime = 0;
+}
